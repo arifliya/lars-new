22041c8c4550059654d33095f954654824b73c75
Arif Liya <arifliya@gmail.com>
2019-11-26 09:16:10 +0000

    new version with filters and restructure of data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,37 +15,61 @@ $('#backLink').on('click', function(e){
 });
 
 
-// autocomlete option select function //
-$(".autocomplete__menu").click(function() {
-  $("#searchProviders").trigger("click");
-  $('#changeUrl').attr('action', "qualification").submit();
+//************** */ autocomlete option select function for version 1-0-0 *****************//
+// $(".autocomplete__menu").click(function() {
+//   $("#searchProviders").trigger("click");
+//   $('#changeUrl').attr('action', "qualification").submit();
 
-  if ($('.autocomplete__input').val() === 'maths') {
-    $('#changeUrl').attr('action', "all-results").submit();
-  }
+//   if ($('.autocomplete__input').val() === 'maths') {
+//     $('#changeUrl').attr('action', "all-results").submit();
+//   }
   
-  if ($('.autocomplete__input').val() === 'science') {
-    $('#changeUrl').attr('action', "all-results").submit();
-  }
+//   if ($('.autocomplete__input').val() === 'science') {
+//     $('#changeUrl').attr('action', "all-results").submit();
+//   }
   
-  if ($('.autocomplete__input').val() === 'AQA (Assessment and Qualifications Alliance)') {
-    $('#changeUrl').attr('action', "all-results").submit();
-  }
+//   if ($('.autocomplete__input').val() === 'AQA (Assessment and Qualifications Alliance)') {
+//     $('#changeUrl').attr('action', "all-results").submit();
+//   }
   
-  if ($('.autocomplete__input').val() === 'Edexcel') {
-    $('#changeUrl').attr('action', "all-results").submit();
-  }
+//   if ($('.autocomplete__input').val() === 'Edexcel') {
+//     $('#changeUrl').attr('action', "all-results").submit();
+//   }
   
+//   if ($('.autocomplete__input').val() === 'engineering') {
+//     $('#changeUrl').attr('action', "all-results").submit();
+//   }
+
+//   if ($('.autocomplete__input').val() === 'Functional Skills qualification in Mathematics') {
+//     $('#changeUrl').attr('action', "all-results").submit();
+//   }
+
+//   // else {
+//   //   $('#changeUrl').attr('action', "all-results").submit();
+//   // }
+
+// });
+
+$("#searchProviders").click(function() {
+  if ($('.autocomplete__input').val() === 'certificate in engineering') {
+    $('#changeUrl').attr('action', "all-results-engineering");
+  }
   if ($('.autocomplete__input').val() === 'engineering') {
-    $('#changeUrl').attr('action', "all-results").submit();
+    $('#changeUrl').attr('action', "all-results-engineering");
   }
-
-  if ($('.autocomplete__input').val() === 'Functional Skills qualification in Mathematics') {
-    $('#changeUrl').attr('action', "all-results").submit();
+  if ($('.autocomplete__input').val() === 'psychology') {
+    $('#changeUrl').attr('action', "all-results-psy");
   }
+  if ($('.autocomplete__input').val() === 'functional skills qualification in mathematics') {
+    $('#changeUrl').attr('action', "all-results-func");
+  }
+  if ($('.autocomplete__input').val() === 'maths') {
+    $('#changeUrl').attr('action', "all-results-func");
+  }
+});
 
-  // else {
-  //   $('#changeUrl').attr('action', "all-results").submit();
-  // }
-
+$('.filter-box-button').click(function(e){
+  $(this).toggleClass('rotate');
+  $(this).next('.filter-box').toggleClass('hidden');
+  e.preventDefault();
 });
