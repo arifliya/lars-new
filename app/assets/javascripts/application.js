02644bcb372bca217a5d297e8c8e7e43fd2c976a
Arif Liya <arifliya@gmail.com>
2019-11-26 10:56:28 +0000

    updated links for app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -68,8 +68,35 @@ $("#searchProviders").click(function() {
   }
 });
 
+
+// expand filters //
 $('.filter-box-button').click(function(e){
   $(this).toggleClass('rotate');
   $(this).next('.filter-box').toggleClass('hidden');
   e.preventDefault();
 });
+
+// // filter functionality for awarding body //
+// $('#awardingBody :checkbox').click(function () {
+//   $('#resultList li').hide();
+//   $('#awardingBody :checkbox:checked').each(function () {
+//     $('.' + $(this).val()).show();
+//   })
+
+//   if (!$('#awardingBody :checkbox').is(':checked')) {
+//     $('#resultList li').show();
+//   }
+// });
+
+
+// // filter functionality for level //
+// $('#level :checkbox').click(function () {
+//   $('#resultList li').hide();
+//   $('#level :checkbox:checked').each(function () {
+//     $('.' + $(this).val()).show();
+//   });
+
+//   if (!$('#level :checkbox').is(':checked')) {
+//     $('#resultList li').show();
+//   }
+// });
